0d566a8a9bd9999bc8bfc6a259bafe3c73053e5f
lousev <louiesevilla21@gmail.com>
2019-08-09 23:10:56 +0800

    redesigned the gui

--- script.js
+++ script.js
@@ -16,132 +16,151 @@ const conversationOutput = document.querySelector("#conversationOutput");
 let attendanceLog;
 let data = localStorage.getItem("storedAttendance");
 if (data) {
-    attendanceLog = JSON.parse(data);
-    loadStoredAttendance(attendanceLog);
+  attendanceLog = JSON.parse(data);
+  loadStoredAttendance(attendanceLog);
 } else {
-    attendanceLog = [];
+  attendanceLog = [];
 }
+
 function loadStoredAttendance(array) {
-    array.forEach(function (item) {
-        addInputs(item.name, item.team, item.time);
-    });
+  array.forEach(function (item) {
+    addInputs(item.name, item.team, item.time);
+  });
 }
 // Local Storage for the conversation log
 let conversationLog;
 let data2 = localStorage.getItem('storedNotes');
 if (data2) {
-    conversationLog = JSON.parse(data2);
-    loadStoredNotes(conversationLog);
+  conversationLog = JSON.parse(data2);
+  loadStoredNotes(conversationLog);
 } else {
-    conversationLog = [];
+  conversationLog = [];
 }
+
 function loadStoredNotes(array) {
-    array.forEach(function (item) {
-        addNotes(item.time, item.notes);
-    });
+  array.forEach(function (item) {
+    addNotes(item.time, item.notes);
+  });
 }
 
 // Load event listeners
 loadEventListeners();
+
 function loadEventListeners() {
-    // Press enter to append the name, team, and time
-    document.addEventListener('keyup', enterInputs);
-    // Clear all inputs
-    clearBtn.addEventListener('click', snap);
-    clearConvo.addEventListener('click', snap2);
-    // Form prevent default submit
-    form.addEventListener('submit', formSubmit);
-    // Conversation Log Area
-    conversationForm.addEventListener('submit', formSubmit);
-    enterNote.addEventListener('click', enterNoteClicked)
+  // Press enter to append the name, team, and time
+  form.addEventListener('keyup', enterInputs);
+  // Clear all inputs
+  clearBtn.addEventListener('click', snap);
+  clearConvo.addEventListener('click', snap2);
+  // Form prevent default submit
+  form.addEventListener('submit', formSubmit);
+  // Conversation Log Area
+  conversationForm.addEventListener('keyup', enterNotes);
 };
 
 function addInputs(name, team, time) {
-    let text = `<li class="collection-item blue lighten-5"><span class="capitalize">${name}</span> <span class="capitalize">(${team})</span> joined the call at ${time}.</li>`
+  let text = `<li class="collection-item"><span class="capitalize">${name}</span> <span class="capitalize">(${team})</span> joined the call at ${time}.</li>`
 
-    let position = "beforeend";
-    textOutput.insertAdjacentHTML(position, text);
+  let position = "beforeend";
+  textOutput.insertAdjacentHTML(position, text);
 };
 // Form prevent default submit
 function formSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 };
 
 function enterInputs(event) {
-    if (event.keyCode == 13) {
-        if (nameInput && nameInput.value && teamInput && teamInput.value) {
-            // declare scope variables
-            let name = nameInput.value;
-            let team = teamInput.value;
-            let options = { hour: 'numeric', minute: 'numeric', second: 'numeric', month: 'numeric', day: 'numeric', year: '2-digit' };
-            let currentTime = new Date();
-            let time = currentTime.toLocaleTimeString('en-US', options);
-            if (name, team, time) {
-                addInputs(name, team, time);
-                attendanceLog.push({
-                    name: name,
-                    team: team,
-                    time: time
-                });
-            }
-            nameInput.value = "";
-            teamInput.value = "";
-            nameInput.focus();
-            // Local Storage
-            localStorage.setItem("storedAttendance", JSON.stringify(attendanceLog));
-        }
-    };
+  if (event.keyCode == 13) {
+    if (nameInput && nameInput.value && teamInput && teamInput.value) {
+      // declare scope variables
+      let name = nameInput.value;
+      let team = teamInput.value;
+      let options = {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        year: '2-digit'
+      };
+      let currentTime = new Date();
+      let time = currentTime.toLocaleTimeString('en-US', options);
+      if (name, team, time) {
+        addInputs(name, team, time);
+        attendanceLog.push({
+          name: name,
+          team: team,
+          time: time
+        });
+      }
+      nameInput.value = "";
+      teamInput.value = "";
+      nameInput.focus();
+      // Local Storage
+      localStorage.setItem("storedAttendance", JSON.stringify(attendanceLog));
+    }
+  };
 };
 // Copy texts to clipboard
 const clipboard = new ClipboardJS("#copy");
 clipboard.on('success', function () {
-    M.toast({
-        html: "Ready to paste",
-        classes: "blue",
-    });
+  M.toast({
+    html: "Ready to paste",
+    classes: "blue",
+  });
 });
 
 // Clear Inputs
 function snap() {
-    let dialog = confirm("Are you sure? There's no recovery.");
-    if (dialog == true) {
-        textOutput.innerHTML = "";
-        localStorage.removeItem("storedAttendance");
-    };
+  let dialog = confirm("Are you sure? There's no recovery.");
+  if (dialog == true) {
+    textOutput.innerHTML = "";
+    localStorage.removeItem("storedAttendance");
+  };
 };
 
 function snap2() {
-    let dialog = confirm("Are you sure? There's no recovery.");
-    if (dialog == true) {
-        conversationOutput.innerHTML = "";
-        localStorage.removeItem("storedNotes");
-    };
+  let dialog = confirm("Are you sure? There's no recovery.");
+  if (dialog == true) {
+    conversationOutput.innerHTML = "";
+    localStorage.removeItem("storedNotes");
+  };
 };
 
-// 
+//
 // Conversation Log Area
-// 
+//
 function addNotes(time, notes) {
-    let text = `<li class="collection-item blue lighten-5">${time} - <span class="">${notes}</span></li>`
-    let position = "beforeend";
-    conversationOutput.insertAdjacentHTML(position, text);
+  let text = `<li class="collection-item">${time} - <span class="">${notes}</span></li>`
+  let position = "beforeend";
+  conversationOutput.insertAdjacentHTML(position, text);
 }
 
-function enterNoteClicked() {
+// Enter notes
+function enterNotes(event) {
+  if (event.keyCode == 13) {
     if (noteArea && noteArea.value) {
-        let notes = noteArea.value;
-        let options = { hour: 'numeric', minute: 'numeric', second: 'numeric', month: 'numeric', day: 'numeric', year: '2-digit' };
-        let currentTime = new Date();
-        let time = currentTime.toLocaleTimeString('en-US', options);
-        if (time, notes) {
-            addNotes(time, notes)
-            conversationLog.push({
-                time: time,
-                notes: notes
-            });
-        }
-        noteArea.value = "";
-        noteArea.focus();
-        localStorage.setItem("storedNotes", JSON.stringify(conversationLog));
+      let notes = noteArea.value;
+      let options = {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        year: '2-digit'
+      };
+      let currentTime = new Date();
+      let time = currentTime.toLocaleTimeString('en-US', options);
+      if (time, notes) {
+        addNotes(time, notes)
+        conversationLog.push({
+          time: time,
+          notes: notes
+        });
+      }
+      noteArea.value = "";
+      noteArea.focus();
+      localStorage.setItem("storedNotes", JSON.stringify(conversationLog));
     }
+  }
 }
