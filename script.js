be865fee44c5c18666b6eadb305061b220c91d9a
lousev <louiesevilla21@gmail.com>
2019-08-17 23:53:24 +0800

    added tippy js function

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Global scope variables
-const nameInput = document.getElementById('nameInput');
-const teamInput = document.getElementById('teamInput');
-const textOutput = document.querySelector('#textOutput');
-const clearBtn = document.querySelector('#clear');
-const clearConvo = document.querySelector('#clearConvo');
-const form = document.querySelector('#form');
-const btnDiv = document.querySelector('.btnDiv');
-const btnDiv2 = document.querySelector('.btnDiv2');
-const conversationForm = document.querySelector('#conversationForm');
-const enterNote = document.querySelector('#enterNote');
-const noteArea = document.querySelector('#textarea');
+const nameInput = document.getElementById("nameInput");
+const teamInput = document.getElementById("teamInput");
+const textOutput = document.querySelector("#textOutput");
+const clearBtn = document.querySelector("#clear");
+const clearConvo = document.querySelector("#clearConvo");
+const form = document.querySelector("#form");
+const btnDiv = document.querySelector(".btnDiv");
+const btnDiv2 = document.querySelector(".btnDiv2");
+const conversationForm = document.querySelector("#conversationForm");
+const enterNote = document.querySelector("#enterNote");
+const noteArea = document.querySelector("#textarea");
 const conversationOutput = document.querySelector("#conversationOutput");
 
 // Local Storage for the attendance log
@@ -23,13 +23,13 @@ if (data) {
 }
 
 function loadStoredAttendance(array) {
-  array.forEach(function (item) {
+  array.forEach(function(item) {
     addInputs(item.name, item.team, item.time);
   });
 }
 // Local Storage for the conversation log
 let conversationLog;
-let data2 = localStorage.getItem('storedNotes');
+let data2 = localStorage.getItem("storedNotes");
 if (data2) {
   conversationLog = JSON.parse(data2);
   loadStoredNotes(conversationLog);
@@ -38,7 +38,7 @@ if (data2) {
 }
 
 function loadStoredNotes(array) {
-  array.forEach(function (item) {
+  array.forEach(function(item) {
     addNotes(item.time, item.notes);
   });
 }
@@ -48,26 +48,26 @@ loadEventListeners();
 
 function loadEventListeners() {
   // Press enter to append the name, team, and time
-  form.addEventListener('keyup', enterInputs);
+  form.addEventListener("keyup", enterInputs);
   // Clear all inputs
-  clearBtn.addEventListener('click', snap);
-  clearConvo.addEventListener('click', snap2);
+  clearBtn.addEventListener("click", snap);
+  clearConvo.addEventListener("click", snap2);
   // Form prevent default submit
-  form.addEventListener('submit', formSubmit);
+  form.addEventListener("submit", formSubmit);
   // Conversation Log Area
-  conversationForm.addEventListener('keyup', enterNotes);
-};
+  conversationForm.addEventListener("keyup", enterNotes);
+}
 
 function addInputs(name, team, time) {
-  let text = `<li class="collection-item"><span class="capitalize">${name}</span> <span class="capitalize">(${team})</span> joined the call at ${time}.</li>`
+  let text = `<li class="collection-item"><span class="capitalize">${name}</span> <span class="capitalize">(${team})</span> joined the call at ${time}.</li>`;
 
   let position = "beforeend";
   textOutput.insertAdjacentHTML(position, text);
-};
+}
 // Form prevent default submit
 function formSubmit(e) {
   e.preventDefault();
-};
+}
 
 function enterInputs(event) {
   if (event.keyCode == 13) {
@@ -76,16 +76,16 @@ function enterInputs(event) {
       let name = nameInput.value;
       let team = teamInput.value;
       let options = {
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-        year: '2-digit'
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        month: "numeric",
+        day: "numeric",
+        year: "2-digit"
       };
       let currentTime = new Date();
-      let time = currentTime.toLocaleTimeString('en-US', options);
-      if (name, team, time) {
+      let time = currentTime.toLocaleTimeString("en-US", options);
+      if ((name, team, time)) {
         addInputs(name, team, time);
         attendanceLog.push({
           name: name,
@@ -99,15 +99,26 @@ function enterInputs(event) {
       // Local Storage
       localStorage.setItem("storedAttendance", JSON.stringify(attendanceLog));
     }
-  };
-};
+  }
+}
+
 // Copy texts to clipboard
 const clipboard = new ClipboardJS("#copy");
-clipboard.on('success', function () {
-  M.toast({
-    html: "Ready to paste",
-    classes: "blue",
-  });
+clipboard.on("success", function() {});
+// Tooltips for copy
+tippy(".copy", {
+  content: "Copy All",
+  placement: "left",
+  arrow: true,
+  arrowType: "sharp"
+});
+
+// Tooltips for Remove
+tippy(".clear", {
+  content: "Clear All",
+  placement: "right",
+  arrow: true,
+  arrowType: "sharp"
 });
 
 // Clear Inputs
@@ -116,22 +127,22 @@ function snap() {
   if (dialog == true) {
     textOutput.innerHTML = "";
     localStorage.removeItem("storedAttendance");
-  };
-};
+  }
+}
 
 function snap2() {
   let dialog = confirm("Are you sure? There's no recovery.");
   if (dialog == true) {
     conversationOutput.innerHTML = "";
     localStorage.removeItem("storedNotes");
-  };
-};
+  }
+}
 
 //
 // Conversation Log Area
 //
 function addNotes(time, notes) {
-  let text = `<li class="collection-item">${time} - <span class="">${notes}</span></li>`
+  let text = `<li class="collection-item">${time} - <span class="">${notes}</span></li>`;
   let position = "beforeend";
   conversationOutput.insertAdjacentHTML(position, text);
 }
@@ -142,17 +153,17 @@ function enterNotes(event) {
     if (noteArea && noteArea.value) {
       let notes = noteArea.value;
       let options = {
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-        year: '2-digit'
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        month: "numeric",
+        day: "numeric",
+        year: "2-digit"
       };
       let currentTime = new Date();
-      let time = currentTime.toLocaleTimeString('en-US', options);
-      if (time, notes) {
-        addNotes(time, notes)
+      let time = currentTime.toLocaleTimeString("en-US", options);
+      if ((time, notes)) {
+        addNotes(time, notes);
         conversationLog.push({
           time: time,
           notes: notes
